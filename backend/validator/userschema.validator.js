ae431276fac8f6f11c2f8b8b5e97ab38061bd5d4
om bhardwaj <ombhardwaj155@gmail.com>
2025-04-28 23:05:00 +0530

    utils added

---
+++ backend/validator/userschema.validator.js
@@ -0,0 +1,36 @@
+import { z } from "zod";
+
+const roleEnum = z.enum(["admin", "user", "guest"]);
+
+const userSchema = z.object({
+  username: z
+    .string({
+      required_error: "username is required",
+      invalid_type_error: "username should be string",
+    })
+    .min(3, { message: "Must be 5 or more characters long" })
+    .trim()
+    .transform((username) => username.toLocaleLowerCase()),
+
+  email: z
+    .string({
+      required_error: "email is required",
+    })
+    .email({ message: "email is not valid" })
+    .trim(),
+
+  password: z
+    .string({
+      required_error: "password is required",
+    })
+    .min(8, { message: "password must be of atleast 8 characters" })
+    .trim()
+    .regex(/[!@#$%^&*]/, {
+      message: "Password must include a special character",
+    })
+    .regex(/[a-z]/, { message: "Password must include a small letter" })
+    .regex(/[A-Z]/, { message: "Password must include a capital letter" })
+    .regex(/\d/, { message: "Password must include a number" }), // [0-9]
+
+  role: z.nativeEnum(roleEnum),
+});
