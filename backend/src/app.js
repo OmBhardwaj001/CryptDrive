b5b58355471b47c35d154f187951b6009c0cf340
Mukul Pal <mukulpal0333@gmail.com>
2025-04-28 11:08:34 +0530

    refactor: refactored the existing code

---
+++ backend/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("welcome to home page");
+});
+
+export default app;
