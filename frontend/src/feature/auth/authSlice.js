66bd5c85c991a66578bec8252c8a1bda42be1a93
om bhardwaj <ombhardwaj155@gmail.com>
2025-09-22 23:27:24 +0530

    auth slice made

--- frontend/src/feature/auth/authSlice.js
+++ frontend/src/feature/auth/authSlice.js
@@ -2,8 +2,18 @@ import React from 'react'
 import {createSlice} from '@reduxjs/toolkit'
 
 const initialState={
-    
-
+    user:{},
+    token:null,
+    isAuthenticated:false,
+    isLoading:false,
+    error:null,
+    registrationData:{
+        username:"",
+        email:"",
+        password:"",
+        fullname:"",
+        avatarFile:null,
+    }    
 }
 
 export const authSlice = createSlice({
@@ -11,6 +21,32 @@ export const authSlice = createSlice({
     initialState,
 
     reducers:{
+        registerStart:(state,action)=>{
+            state.isLoading =true
+            state.error = false
+        },
+        registerSuccess:(state,action)=>{
+            state.user = action.payload.user
+            state.token = action.payload.token
+            state.isLoading = false;
+            state.error = false
+        },
+        registerFailure:(state,action)=>{
+            state.error = action.payload
+            state.isLoading = false
+            state.isAuthenticated = false
+        },
+        setAvatar:(state,action)=>{
+            const{file} = action.payload
+            state.registrationData.avatarFile = file
+        },
+        updateRegistrationField:(state,action)=>{
+            const {field, value} = action.payload
+            state.registrationData[field] = value
+        }
 
     }
 })
+
+export const {registerStart,registerSuccess,registerFailure,setAvatar,updateRegistrationField} = authSlice.actions
+export default authSlice.reducer
