7b8e54b1ced340846d73ef5cef61d2a10d04e118
om bhardwaj <ombhardwaj155@gmail.com>
2025-09-22 23:27:55 +0530

    added rtk and router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,22 @@
-import Login from "./components/auth/Login"
-import SignupPage from './components/auth/Signup'
-import ForgotPasswordPage from './components/auth/ForgotPassword'
-import ResetPasswordPage from './components/auth/ResetPassword'
+import React from 'react'
+import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from 'react-router-dom'
+import {Login,Register} from './components/auth/Index'
+import AuthLayout from './layouts/AuthLayout'
+
 
 function App() {
-    return(
-        <>
-            <Login/>
-            <SignupPage/>
-            <ForgotPasswordPage/>
-            <ResetPasswordPage/>
-        </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path='/' element={<AuthLayout/>}>
+        <Route path='/' element={<Register/>} />
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        </Route>
+      </>
     )
-
+  )
+  return <RouterProvider router={router}/>
 }
 
 export default App
